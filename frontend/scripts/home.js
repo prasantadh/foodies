ed5e9410aeca2546b8087e7bf4a368d92b5d659c
siddharthchd <siddharth.chaudhary1705@gmail.com>
2021-05-09 18:48:09 -0400

    fixed carousel and added js for recipe population

--- frontend/scripts/home.js
+++ frontend/scripts/home.js
@@ -1,46 +1,27 @@
-var slider = document.getElementById("rowSlider");
+const popularRecipesBlock = document.getElementById('popularRecipes');
 
-for (let i = 0; i < 10; i ++) {
-//     var colDiv = document.createElement('div');
-//     colDiv.className = "col s3";
-//     var cardDiv = document.createElement('div');
-//     cardDiv.className = "card";
-//     var cardImageDiv = document.createElement('div');
-//     cardImageDiv.className = "card-image";
-     var cardImage = document.createElement('img');
-     cardImage.src = "images/burger.jpg";
-//     var cardImageSpan = document.createElement('span');
-//     cardImageSpan.className = "card-title";
-//     cardImageSpan.textContent = "Burger";
-//     var cardContentDiv = document.createElement('div');
-//     cardContentDiv.className = "card-content";
-//     var cardP = document.createElement('p');
-//     cardP.innerHTML = "Prep Time : 10 minutes.";
+function addCard() {
+    var recipeName = "Burger";
+    var recipeImage = "images/burger.jpg";
+    var recipePrepTime = "Prep Time : 10 mins";
 
-//     slider.appendChild(colDiv);
-//     slider.appendChild(cardDiv);
-//     slider.appendChild(cardImageDiv);
-//     slider.appendChild(cardImage);
-//     slider.appendChild(cardImageSpan);
-//     slider.appendChild(cardContentDiv);
-//     slider.appendChild(cardP);
+    var colDiv = document.createElement('div');
+    colDiv.className = "col s3";
+    colDiv.innerHTML = `
+        <div class="card">
+            <div class="card-image">
+                <img src="` + recipeImage + `">
+                <span class="card-title">` + recipeName + `</span>
+            </div>
+            <div class="card-content">
+                <p>` + recipePrepTime + `</p>
+            </div>
+        </div>
+    `;
 
+    return colDiv
 }
 
-
-
-const div = document.createElement('div');
-div.className = 'col s3';
-div.innerHTML = `
-    <div class="card">
-        <div class="card-image">
-            <img src="images/burger.jpg">
-            <span class="card-title">Burger</span>
-        </div>
-        <div class="card-content">
-            <p>Prep Time: 10 mins</p>
-        </div>
-    </div>
-`;
-  
-slider.appendChild(div);
+for (var i = 0; i < 10; i++) {
+    popularRecipesBlock.appendChild(addCard());
+}
