be534038bf5b8d05c60649926015a9d0f3c033d5
siddharthchd <siddharth.chaudhary1705@gmail.com>
2021-05-10 21:07:12 -0400

    cognito authentication

---
+++ frontend/scripts/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_WOWiGWkNP', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '52mveimcink6t1osd3fopgfekn' //is this used anywhere?
+    },
+};
