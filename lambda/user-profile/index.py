c759bf57f7d2a06e012eb706355167bfcd496591
Prasant Adhikari <pa1038@nyu.edu>
2021-05-13 15:05:27 -0400

    fix syntax errors on lambda

--- lambda/user-profile/index.py
+++ lambda/user-profile/index.py
@@ -1,4 +1,4 @@
 def lambda_handler(event, context):
-    print("event" -> event)
-    print("context" -> context)
+    print("event ->", event)
+    print("context ->", context)
     return {"statusCode": "200", "body" : "hello from user profile"}
