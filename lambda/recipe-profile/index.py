a34c5901a9cde405ea65554578660ce32d3dfec4
Ana-stasiaa <62129183+Ana-stasiaa@users.noreply.github.com>
2021-05-14 00:41:12 -0400

    reads from dynamo outputs recipe page

--- lambda/recipe-profile/index.py
+++ lambda/recipe-profile/index.py
@@ -1,4 +1,24 @@
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
 def lambda_handler(event, context):
-    print("event -> ", event)
-    print("context -> ", context)
-    return {"statusCode": "200", "body" : "hello from recipe profile"}
+    ##print()
+    dynamodb_client = boto3.client('dynamodb', region_name="us-east-1")
+    dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
+    table = dynamodb.Table('recipes')
+
+    id = event['path'][9:]
+    response = dynamodb_client.get_item(
+    TableName='recipes',
+    Key={
+        'id': {'S': id}
+    }
+)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response),
+        'headers': 
+            {'Access-Control-Allow-Origin': '*'}
+    }
