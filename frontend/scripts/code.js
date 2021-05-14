44c1c24ce15505838be2c0f6f8178251b32c1b77
Ana-stasiaa <62129183+Ana-stasiaa@users.noreply.github.com>
2021-05-14 08:14:50 -0400

    added comments and likes

--- frontend/scripts/code.js
+++ frontend/scripts/code.js
@@ -1,30 +1,55 @@
 var apigClient = apigClientFactory.newClient();
 
 function getRecipe(){
-    apigClient.recipesIdGet(params = { "id": "p3pKOD6jIHEcjf20CCXohP8uqkG5dGi" , "table": "recipes"}, '').then(function (result) {
+    apigClient.recipesIdGet(params = { "id": "0MQI9HFNFlFGl2v26NcAaqegwGIavbq"}, "").then(function (result) {
+        
+        //Title
         var recTitle = result['data']['Item']['title']['S']
-
         console.log(recTitle)
-        console.log(recIngredients)
-        console.log(recInstructions)
         if (document.getElementById('RecipeTitle')) {
             document.getElementById('RecipeTitle').innerText = recTitle;
         }
+        
+        //Ingredients
         var ingredientLength = result['data']['Item']['ingredients']['L']['length']
         var i;
         var recIngredients = '';
         for (i=0; i<ingredientLength; i++){
             recIngredients+=result['data']['Item']['ingredients']['L'][i]['S']
-
             recIngredients+="\r\n"
         }
-        
+        console.log(recIngredients)
         if (document.getElementById('Ingredients')) {
             document.getElementById('Ingredients').innerText = recIngredients;
         }
+        
+        //Instructions
         var recInstructions = result['data']['Item']['instructions']['S']
+        console.log(recInstructions)
         if (document.getElementById('Instructions')) {
             document.getElementById('Instructions').innerText = recInstructions;
+        }
+
+        //Likes
+        var likes = result['data']['Item']['likes']['L']['length'] + ' Likes'
+        console.log(likes)
+        if (document.getElementById('likeButton')) {
+            document.getElementById('likeButton').innerText = likes;
+        }
+
+        var commentLength = result['data']['Item']['comments']['L']['length'];
+        var i;
+        var comments = ''
+        for (i=0; i<commentLength; i++){
+
+            comments+=result['data']['Item']['comments']['L'][i]['L'][1]['S']
+            comments+="\r\n"
+            comments+=result['data']['Item']['comments']['L'][i]['L'][2]['S']
+            comments+="\r\n\r\n"
+        }
+        console.log(comments)
+        if (document.getElementById('Comments')) {
+            document.getElementById('Comments').innerText = comments;
         }
     })
 }
